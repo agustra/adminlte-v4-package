3db321ba9accd2d50733fd5722a2bce7f192e82c
Agus Syahputra <agus.aishiteruu@gmail.com>
2025-09-04 06:17:32 +0700

    Add Bootstrap Icons CDN to login and register pages

--- src/Resources/views/examples/login.blade.php
+++ src/Resources/views/examples/login.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>AdminLTE 4 | Login Page Example</title>
     <link rel="stylesheet" href="{{ asset('vendor/adminlte/css/adminlte-css.css') }}">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.css">
 </head>
 <body class="login-page bg-body-secondary">
     <div class="login-box">
