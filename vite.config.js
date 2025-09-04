4d8d880a7c9b4f6b43a68859d1158c3aeb23c8a8
Agus Syahputra <agus.aishiteruu@gmail.com>
2025-09-04 11:47:47 +0700

    origin main

--- vite.config.js
+++ vite.config.js
@@ -1,43 +1,95 @@
-import { defineConfig } from 'vite';
+// import { defineConfig } from 'vite';
+
+// export default defineConfig({
+//     build: {
+//         outDir: 'dist',
+//         rollupOptions: {
+//             input: {
+//                 adminlte: 'resources/js/adminlte.js',
+//                 'adminlte-css': 'resources/css/adminlte.scss'
+//             },
+//             output: {
+//                 entryFileNames: 'js/[name].js',
+//                 chunkFileNames: 'js/[name].js',
+//                 assetFileNames: (assetInfo) => {
+//                     if (assetInfo.name.endsWith('.css')) {
+//                         return 'css/[name][extname]';
+//                     }
+//                     if (assetInfo.name.match(/\.(woff2?|eot|ttf|otf)$/)) {
+//                         return 'fonts/[name][extname]';
+//                     }
+//                     return 'assets/[name][extname]';
+//                 }
+//             }
+//         },
+//         assetsInlineLimit: 0
+//     },
+//     experimental: {
+//         renderBuiltUrl(filename, { hostType }) {
+//             if (hostType === 'css' && filename.match(/\.(woff2?|eot|ttf|otf)$/)) {
+//                 return `../fonts/${filename}`;
+//             }
+//             return { relative: true };
+//         }
+//     },
+//     css: {
+//         preprocessorOptions: {
+//             scss: {
+//                 api: 'modern-compiler',
+//                 silenceDeprecations: ['legacy-js-api', 'import', 'global-builtin', 'color-functions']
+//             }
+//         }
+//     }
+// });
+
+import { defineConfig } from "vite";
 
 export default defineConfig({
-    build: {
-        outDir: 'dist',
-        rollupOptions: {
-            input: {
-                adminlte: 'resources/js/adminlte.js',
-                'adminlte-css': 'resources/css/adminlte.scss'
-            },
-            output: {
-                entryFileNames: 'js/[name].js',
-                chunkFileNames: 'js/[name].js',
-                assetFileNames: (assetInfo) => {
-                    if (assetInfo.name.endsWith('.css')) {
-                        return 'css/[name][extname]';
-                    }
-                    if (assetInfo.name.match(/\.(woff2?|eot|ttf|otf)$/)) {
-                        return 'fonts/[name][extname]';
-                    }
-                    return 'assets/[name][extname]';
-                }
-            }
+  base: "./", // penting! agar semua asset pakai path relatif, bukan absolute /
+  build: {
+    outDir: "dist",
+    rollupOptions: {
+      input: {
+        adminlte: "resources/js/adminlte.js",
+        "adminlte-css": "resources/css/adminlte.scss",
+      },
+      output: {
+        entryFileNames: "js/[name].js",
+        chunkFileNames: "js/[name].js",
+        assetFileNames: (assetInfo) => {
+          if (assetInfo.name.endsWith(".css")) {
+            return "css/[name][extname]";
+          }
+          if (assetInfo.name.match(/\.(woff2?|eot|ttf|otf)$/)) {
+            return "fonts/[name][extname]";
+          }
+          return "assets/[name][extname]";
         },
-        assetsInlineLimit: 0
+      },
     },
-    experimental: {
-        renderBuiltUrl(filename, { hostType }) {
-            if (hostType === 'css' && filename.match(/\.(woff2?|eot|ttf|otf)$/)) {
-                return `../fonts/${filename}`;
-            }
-            return { relative: true };
-        }
+    assetsInlineLimit: 0,
+  },
+  experimental: {
+    renderBuiltUrl(filename, { hostType }) {
+      // Jika dipanggil dari CSS dan berupa font
+      if (hostType === "css" && filename.match(/\.(woff2?|eot|ttf|otf)$/)) {
+        return { relative: true }; // hasil jadi ../fonts/xxx
+      }
+      // fallback: semua tetap relatif
+      return { relative: true };
     },
-    css: {
-        preprocessorOptions: {
-            scss: {
-                api: 'modern-compiler',
-                silenceDeprecations: ['legacy-js-api', 'import', 'global-builtin', 'color-functions']
-            }
-        }
-    }
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        api: "modern-compiler",
+        silenceDeprecations: [
+          "legacy-js-api",
+          "import",
+          "global-builtin",
+          "color-functions",
+        ],
+      },
+    },
+  },
 });
