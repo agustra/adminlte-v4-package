d6b5741e254deb4ca617a43cfd2b8b8a56289f36
Agus Syahputra <agus.aishiteruu@gmail.com>
2025-09-04 12:40:15 +0700

    Remove CDN Bootstrap Icons, use bundled assets only - v1.0.0 final

--- src/Resources/views/examples/register.blade.php
+++ src/Resources/views/examples/register.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>AdminLTE 4 | Register Page Example</title>
     <link rel="stylesheet" href="{{ asset('vendor/adminlte/css/adminlte-css.css') }}">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.css">
+
 </head>
 <body class="register-page bg-body-secondary">
     <div class="register-box">
--- src/Resources/views/layouts/app.blade.php
+++ src/Resources/views/layouts/app.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=yes" />
     
     <!--begin::AdminLTE Assets-->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.0/font/bootstrap-icons.css">
+
     <link rel="stylesheet" href="{{ asset('vendor/adminlte/css/adminlte-css.css') }}">
     <!--end::AdminLTE Assets-->
     
