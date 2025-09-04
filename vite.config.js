b0a6d7542044f652cdf76a98c845f77c9158577d
Agus Syahputra <agus.aishiteruu@gmail.com>
2025-09-04 11:06:35 +0700

    Simplify vite config to prevent nested vendor folders

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,4 @@
 import { defineConfig } from 'vite';
-import { resolve } from 'path';
-import { copyFileSync, existsSync, mkdirSync } from 'fs';
 
 export default defineConfig({
     build: {
@@ -32,38 +30,6 @@ export default defineConfig({
                 api: 'modern-compiler',
                 silenceDeprecations: ['legacy-js-api', 'import', 'global-builtin', 'color-functions']
             }
-        }
-    },
-    plugins: [
-        {
-            name: 'copy-bootstrap-icons',
-            writeBundle() {
-                // Copy Bootstrap Icons fonts
-                const fontsDir = resolve(__dirname, 'dist/fonts');
-                if (!existsSync(fontsDir)) {
-                    mkdirSync(fontsDir, { recursive: true });
-                }
-                
-                const fontFiles = [
-                    'node_modules/bootstrap-icons/font/fonts/bootstrap-icons.woff2',
-                    'node_modules/bootstrap-icons/font/fonts/bootstrap-icons.woff'
-                ];
-                
-                fontFiles.forEach(file => {
-                    if (existsSync(file)) {
-                        const fileName = file.split('/').pop();
-                        copyFileSync(file, resolve(fontsDir, fileName));
-                    }
-                });
-            }
-        }
-    ],
-    experimental: {
-        renderBuiltUrl(filename, { hostType }) {
-            if (hostType === 'css') {
-                return filename;
-            }
-            return { relative: true };
         }
     }
 });
